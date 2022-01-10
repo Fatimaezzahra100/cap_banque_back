889f828bbf45656ded04bba29d3d0b78c2fb32d1
Fatima ELGHANEMY <fatimaezzahra.elg@gmail.com>
2022-01-10 09:41:15 +0100

    initialisation du projet

---
+++ src/test/java/com/capBanque/capBanque/CapBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capBanque.capBanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
