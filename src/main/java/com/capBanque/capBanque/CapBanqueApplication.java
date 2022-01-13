27c19ac8eb576f386a84f449423c9d552343ea0c
Fatima ELGHANEMY <fatimaezzahra.elg@gmail.com>
2022-01-13 15:01:58 +0100

    cross origin

--- src/main/java/com/capBanque/capBanque/CapBanqueApplication.java
+++ src/main/java/com/capBanque/capBanque/CapBanqueApplication.java
@@ -2,12 +2,33 @@ package com.capBanque.capBanque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CapBanqueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapBanqueApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorisation", "Origin, Accept", "X-Requested-With"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorisation",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
--- src/main/java/com/capBanque/capBanque/controller/bankController.java
+++ src/main/java/com/capBanque/capBanque/controller/bankController.java
@@ -3,7 +3,6 @@ package com.capBanque.capBanque.controller;
 import com.capBanque.capBanque.model.Compte;
 import com.capBanque.capBanque.model.CompteCourant;
 import com.capBanque.capBanque.model.CompteEpargne;
-import com.capBanque.capBanque.repository.CompteRepository;
 import com.capBanque.capBanque.service.CompteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,47 +19,47 @@ public class bankController {
     @Autowired
     CompteService compteService;
 
-    @PostMapping("/savecc")
+    @PostMapping("comptes/savecc")
     public ResponseEntity<Compte> addNewCompteCourant(@RequestBody CompteCourant compte) {
         Compte compte1 = compteService.saveCompteCourant(compte);
         return new ResponseEntity<>(compte1, HttpStatus.CREATED);
     }
 
-    @PostMapping("/savece")
-    public ResponseEntity<Compte> addNewCompteCourant(@RequestBody CompteEpargne compte) {
+    @PostMapping("comptes/savece")
+    public ResponseEntity<Compte> addNewCompteEpargne(@RequestBody CompteEpargne compte) {
         Compte compte1 = compteService.saveCompteEpagne(compte);
         return new ResponseEntity<>(compte1, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("comptes/{id}")
     public ResponseEntity<Compte> getCompteById(@PathVariable("id") Long id) {
         Compte compte = compteService.findCompteById(id);
         return new ResponseEntity<Compte>(compte, HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("comptes/delete")
     public void deleteC(@RequestBody Compte compte) {
         compteService.deleteCompte(compte);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("comptes/delete/{id}")
     public void deleteC(@PathVariable("id") Long id) {
         compteService.deleteCompteById(id);
     }
 
-    @GetMapping("/all")
+    @GetMapping("comptes/all")
     public ResponseEntity<List<Compte>> getAllComptes() {
         List<Compte> comptes = compteService.findAllCompte();
         return new ResponseEntity<>(comptes, HttpStatus.OK);
     }
 
-    @PutMapping("/updatecc/{id}")
+    @PutMapping("comptes/updatecc/{id}")
     public ResponseEntity<Compte> updateCC(@RequestBody CompteCourant compte, @PathVariable("id") Long id) {
         Compte compte1 = compteService.updateCompteCourant(compte, id);
         return new ResponseEntity<>(compte1, HttpStatus.OK);
     }
 
-    @PutMapping("/updatece/{id}")
+    @PutMapping("comptes/updatece/{id}")
     public ResponseEntity<Compte> updateCE(@RequestBody CompteEpargne compte, @PathVariable("id") Long id) {
         Compte compte1 = compteService.updateCompteEpargne(compte, id);
         return new ResponseEntity<>(compte1, HttpStatus.OK);
